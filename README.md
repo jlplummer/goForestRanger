b50dfbd5905207de1c373f18e0e3500dea4fbdf5
Jeremiah Plummer <5714388+jlplummer@users.noreply.github.com>
2023-06-05 20:24:46 -0400

    Basic Game struct skeleton in place

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # goForestRanger
 
+## What is this?
 Converting my Ruby game to Go as a learning experience.
 
 [ForestRanger - Ruby](https://github.com/jlplummer/ForestRanger)
+
+## Roadmap
+v0.1 - Convert existing game to Go and Ebitengine
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"log"
+
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	ScreenWidth  = 600
+	ScreenHeight = 300
+)
+
+var (
+	RangerImage *ebiten.Image
+)
+
+func init() {
+	var err error
+	RangerImage, _, err = ebitenutil.NewImageFromFile("media/ranger.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	screen.DrawImage(RangerImage, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ScreenWidth, ScreenHeight
+}
+
 func main() {
-	panic("Implement me")
+	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
+	ebiten.SetWindowTitle("goForestRanger")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
